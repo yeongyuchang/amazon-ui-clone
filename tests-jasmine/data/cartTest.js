acfa8d46a26962fb9cdfb87fe521ae865c29f25c
Yeongyu Chang <chyg0119@gmail.com>
2025-06-11 05:26:58 +0900

    Create automated tests for orderSummary

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart, cart, loadFromStorage, updateDeliveryOption} from "../../data/cart.js";
+import {addToCart, cart, loadFromStorage} from "../../data/cart.js";
 
 describe('test suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
