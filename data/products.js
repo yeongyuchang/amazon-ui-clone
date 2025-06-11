9c2890ee4c50f78d0b143f7dc21f0f8a54166869
Yeongyu Chang <chyg0119@gmail.com>
2025-06-12 08:24:07 +0900

    Handling unexpected errors

--- data/products.js
+++ data/products.js
@@ -74,6 +74,8 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -96,6 +98,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
